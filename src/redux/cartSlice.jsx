a4327a98a53cba61f1455cd7d4add6581ba7462b
arpittiwari <arpittiwari0811@gmail.com>
2024-09-20 18:06:32 +0530

    Refactor HomePageProductCard component to integrate Redux for add to cart functionality

---
+++ src/redux/cartSlice.jsx
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = JSON.parse(localStorage.getItem("cart")) ?? [];
+
+export const cartSlice = createSlice({
+  name: "cart",  
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      state.push(action.payload); 
+    },
+    deleteFromCart: (state, action) => {
+      // Return a new filtered array
+      return state.filter((item) => item.id !== action.payload.id);
+    },
+    incrementQuantity: (state, action) => {
+      const item = state.find((item) => item.id === action.payload.id);
+      if (item) {
+        item.quantity += 1; 
+      }
+    },
+    decrementQuantity: (state, action) => {
+      const item = state.find((item) => item.id === action.payload.id);
+      if (item && item.quantity > 1) {
+        item.quantity -= 1;
+      }
+    },
+  },
+});
+
+export const { addToCart, deleteFromCart, incrementQuantity, decrementQuantity } = cartSlice.actions;
+export default cartSlice.reducer;
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit"; 
+import cartSlice from "./cartSlice";
 
 export const store = configureStore({  
-    reducer: {},
+    reducer: {
+
+        card : cartSlice,
+    },
 });
