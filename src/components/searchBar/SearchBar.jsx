deac70f2511a5b681cffcfac73aa561f3d45b808
arpittiwari <arpittiwari0811@gmail.com>
2024-09-24 19:11:32 +0530

    Refactor UserDetails component to include getAllUser functionality Refactor SearchBar component to use getAllProduct for search functionality Update MyState context to include getAllUser function Update AdminDashboard component to display total number of users

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,43 +1,18 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import MyContext from "../../context/myContext";
+import { useNavigate } from "react-router-dom";
 
-// Search Data
-const searchData = [
-  {
-      name: 'Fashion',
-      image: 'https://i.pinimg.com/564x/3e/05/ce/3e05cefbc7eec79ac175ea8490a67939.jpg'
-  },
-  {
-      name: 'Shirt',
-      image: 'https://i.pinimg.com/736x/e4/61/f2/e461f2246b6ad93e2099d98780626396.jpg'
-  },
-  {
-      name: 'Jacket',
-      image: 'https://i.pinimg.com/564x/fd/50/68/fd50688767adb47aba7204f034554cbd.jpg'
-  },
-  {
-      name: 'Mobile',
-      image: 'https://i.pinimg.com/564x/22/80/8d/22808d88ada424962f2e064f3075b2d1.jpg'
-  },
-  {
-      name: 'Laptop',
-      image: 'https://i.pinimg.com/564x/3e/05/ce/3e05cefbc7eec79ac175ea8490a67939.jpg'
-  },
-  {
-      name: 'Home',
-      image: 'https://i.pinimg.com/736x/e4/61/f2/e461f2246b6ad93e2099d98780626396.jpg'
-  },
-  {
-      name: 'book',
-      image: 'https://i.pinimg.com/564x/fd/50/68/fd50688767adb47aba7204f034554cbd.jpg'
-  },
-]
 
 const SearchBar = () => {
+    const context = useContext(MyContext);
+    const { getAllProduct } = context;
    // Search State 
    const [search, setSearch] = useState("");
 
+   const navigate = useNavigate();
+
    // Filter Search Data
-   const filterSearchData = searchData.filter((obj) => obj.name.toLowerCase().includes(search)).slice(0, 8)
+   const filterSearchData = getAllProduct.filter((obj) => obj.title.toLowerCase().includes(search)).slice(0, 8)
   return (
     <div className="">
     {/* search input  */}
@@ -45,6 +20,7 @@ const SearchBar = () => {
         <input
             type="text"
             placeholder='Search here'
+            value={search}
             onChange={(e) => setSearch(e.target.value)}
             className=' bg-gray-200 placeholder-gray-400 rounded-lg px-2 py-2 w-96 lg:w-96 md:w-96 outline-none text-black '
         />
@@ -58,9 +34,11 @@ const SearchBar = () => {
                     {filterSearchData.map((item, index) => {
                         return (
                             <div key={index} className="py-2 px-2">
-                                <div className="flex items-center gap-2">
-                                    <img className="w-10" src={item.image} alt="" />
-                                    {item.name}
+                                <div className="flex items-center gap-2"
+                                onClick={() => navigate(`/productInfo/${item.id}`)}
+                                >
+                                    <img className="w-10" src={item.productImageUrl} alt="img" />
+                                    {item.title}
                                 </div>
                             </div>
                         )
